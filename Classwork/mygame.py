689f3eccbe36afefc0fb3ea187d2a73abab9a8d1
BostonCruzRuiz <bostoncruzruiz699@gmail.com>
2024-10-03 13:14:21 -0400

    It was display

--- Classwork/mygame.py
+++ Classwork/mygame.py
@@ -6,6 +6,7 @@ pygame.display.set_caption('Pygame Game')
 
 WHITE = (255, 255, 255)
 
+FPS = 60
 
 def draw_windows():
     WIN.fill(WHITE)
@@ -16,11 +17,13 @@ def main():
     run = True
     while run:
         for event in pygame.event.get():
-            if event.tpye == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 run = False
+
+        draw_windows()
 
     pygame.quit()
 
 
-    if __name__=='__main__':
-        main()
+if __name__=='__main__':
+    main()
