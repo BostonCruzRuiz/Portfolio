96a3fa92fe8e6c04dfaa648fb5ccb7ed7b8c0773
Boston Cruz Ruiz <bostoncruzruiz699@gmail.com>
2025-02-28 11:45:45 -0500

    It py

---
+++ Classwork/debuger.py
@@ -0,0 +1,10 @@
+def main():
+    if __name__ == "__main__":
+for i in range(41):
+    if 1 % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif 1 % 3 == 0:
+        print("Fizz")
+    elif 1 % 5 == 0:
+        print("Buzz")
+    else: print(i)
