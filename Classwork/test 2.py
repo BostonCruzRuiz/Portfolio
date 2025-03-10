32a07f6fe967f942b37c983e78a0e9ccf367bd5f
Boston Cruz Ruiz <bostoncruzruiz699@gmail.com>
2025-03-10 08:30:29 -0400

    idk

--- Classwork/test 2.py
+++ Classwork/test 2.py
@@ -1,3 +1,27 @@
+class Car:
+   def __init__(self, brand, model):
+       self.brand = brand
+       self.model = model
+       self.doors = 4
+
+   def print_car(self):
+       print(f"I am a {self.brand} {self.model}.")
+
+
+class Pickup(Car):
+   def __init__(self, brand, model):
+       super().__init__(brand, model) #first use of super()
+       self.doors = 2
+
+   def print_pickup(self):
+       self.print_car() #note what self contains here.
+       print(f"I am a pickup truck and I have {self.doors} doors.")
+
+
 def main():
-    pass
-(main())
+   f250 = Pickup("Ford", "F-250")
+   f250.print_pickup()
+
+
+if __name__ == '__main__':
+   main()
