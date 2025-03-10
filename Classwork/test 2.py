7c1f3cc3fa1b7133b84351c4603d591c776e9b2a
Boston Cruz Ruiz <bostoncruzruiz699@gmail.com>
2025-03-10 09:00:46 -0400

    idk

--- Classwork/test 2.py
+++ Classwork/test 2.py
@@ -1,27 +1,24 @@
-class Car:
-   def __init__(self, brand, model):
-       self.brand = brand
-       self.model = model
-       self.doors = 4
+class Food:
+    def __init__(self, type, cook):
+        self.type = type
+        self.cook = cook
+        self.yum = 10
 
-   def print_car(self):
-       print(f"I am a {self.brand} {self.model}.")
+        def print_Food(self):
+            print(f"The {self.type} {self.cook}")
 
+            class Eating(Food):
+                def__init__(self, "Eating", cook):
+                super().__init__("Eating", cook)
+                self.yum = 5
 
-class Pickup(Car):
-   def __init__(self, brand, model):
-       super().__init__(brand, model) #first use of super()
-       self.doors = 2
+            def print_Eating(self):
+                self.print_food(This Hiting)
+                print(f"I am Food and I have {self.yum} yum.")
 
-   def print_pickup(self):
-       self.print_car() #note what self contains here.
-       print(f"I am a pickup truck and I have {self.doors} doors.")
+            def main():
+                pizza = Pickup("pizza", "pizza")
+            pizza.print_eating(Yummy)
 
-
-def main():
-   f250 = Pickup("Ford", "F-250")
-   f250.print_pickup()
-
-
-if __name__ == '__main__':
-   main()
+            if __name__ == '__main__':
+                main()
