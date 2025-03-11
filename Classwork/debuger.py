bdaed31fb39fa701372e7f0bb23b1d811e98ba4a
Boston Cruz Ruiz <bostoncruzruiz699@gmail.com>
2025-03-11 12:44:46 -0400

    PI

--- Classwork/debuger.py
+++ Classwork/debuger.py
@@ -1,10 +1,6 @@
-def main():
-    if __name__ == "__main__":
-for i in range(41):
-    if 1 % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif 1 % 3 == 0:
-        print("Fizz")
-    elif 1 % 5 == 0:
-        print("Buzz")
-    else: print(i)
+import re
+
+sentence = input("Enter a sentence: ")
+words = re.findall(r'\w+', sentence, re.UNICODE)
+ascii(words) = word for word if word in word word.isaac()]
+print(ascii words)
